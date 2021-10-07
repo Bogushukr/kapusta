a6a485e759147f623f34aeaf8e2d2cbdaf9d5e54
ivanberezin <58605673+ivanberezin@users.noreply.github.com>
2021-10-07 18:55:44 +0300

    2021-10-07

--- src/Sections/SectionExpense/SectionExpense.jsx
+++ src/Sections/SectionExpense/SectionExpense.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
-// import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
 
-const SectionExpense = () => {
-    return (
-        // <div>
-        //     <Link to='/expense' />
-        //     <Link to='/income' />
-        // </div>
-        <>
-            <Calendar />
-            <form onSubmit={handleSubmit}>
-                <input placeholder='Описание товара' />
-                <p>
-                    <select size='1' name='Категория товара'>
-                        <option>Транспорт</option>
-                        <option>Продукты</option>
-                        <option>Здоровье</option>
-                    </select>
-                </p>
-                <input placeholder='0.00' onChange={handleChange} />
-                <Calculator />
-                <button type='submit'>Ввод</button>
-                <button>Очистить</button>
-            </form>
-        </>
-    )
+const INITIAL_STATE = {
+    description: '',
+    category: '',
+    sum: ''
 }
 
-export default SectionExpense
+export default class SectionExpense extends Component {
+
+    static defaultProps = {}
+
+    state = {...INITIAL_STATE}
+
+    handleChange = e => {
+        const {name, value} = e.target
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.onSubmit( {...this.state} )
+        this.reset()
+    }
+
+    reset = () => this.setState( {...INITIAL_STATE} )
+
+    render() {
+        const { description, category, sum } = this.state
+        return (
+            <>
+                <Calendar />
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='Описание товара' name='description' value={description} onChange={this.handleChange} required/>
+                    <p>
+                        <select size='1' name='Категория товара' value={category} onChange={this.handleChange} required>
+                            <option>Транспорт</option>
+                            <option>Продукты</option>
+                            <option>Здоровье</option>
+                        </select>
+                    </p>
+                    <input placeholder='0.00' value={sum} onChange={this.handleChange} onChange={this.handleChange} required/>
+                    <Calculator />
+                    <button type='submit'>Ввод</button>
+                    <button>Очистить</button>
+                    <TableSection />
+                    <Summary />
+                </form>
+            </>
+        )
+    }
+}
--- src/Sections/SectionIncome/SectionIncome.jsx
+++ src/Sections/SectionIncome/SectionIncome.jsx
@@ -3,10 +3,6 @@ import React from 'react'
 
 const SectionIncome = () => {
     return (
-        // <div>
-        //     <Link to='/expense' />
-        //     <Link to='/income' />
-        // </div>
         <>
             <Calendar />
             <form onSubmit={handleSubmit}>
