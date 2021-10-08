271734f649364b5e5ab3840afe4dd185b2bedbca
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-10-08 18:50:29 +0300

    Adds the RegisterForm component

--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
@@ -11,29 +13,44 @@ const validationSchema = Yup.object({
     .required('это обязательное поле'),
 });
 
-export default function RegisterForm({ onSubmit }) {
+export default function RegisterForm({ handleSubmitRegister }) {
   return (
     <Formik
-      initialValues={{ email: '', password: '' }}
+      initialValues={{ name: 'User', email: '', password: '' }}
       validationSchema={validationSchema}
       onSubmit={(values, { setSubmitting, resetForm }) => {
-        console.log(values);
-        onSubmit(values);
+        handleSubmitRegister = handleSubmitRegister(values);
         setSubmitting(false);
         resetForm();
       }}
     >
       <Form autoComplete="off">
         <label htmlFor="email">Электронная почта: </label>
-        <Field type="email" name="email" placeholder="your@email.com" />
+        <br />
+        <Field
+          type="email"
+          name="email"
+          placeholder="your@email.com"
+          onChenge="submit"
+        />
+        <br />
         <ErrorMessage name="email" />
-
+        <br />
         <label htmlFor="password">Пароль: </label>
-        <Field type="password" name="password" placeholder="******" />
+        <br />
+        <Field
+          type="password"
+          name="password"
+          placeholder="******"
+          onChenge="submit"
+        />
+        <br />
         <ErrorMessage name="password" />
-
-        <button type="submit">Войти</button>
+        <br />
         <button type="submit">Регистрация</button>
+        <NavLink to="/" exact>
+          Войти
+        </NavLink>
       </Form>
     </Formik>
   );
