ec9d1fbcd8cfac6dc76e8359543eff05d3d98af1
Anatolii Yarovyi <apyarovyi@gmail.com>
2021-10-10 02:12:40 +0300

    Adds a style for LoginForm

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,14 +3,16 @@ import { NavLink } from 'react-router-dom';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+import './LoginForm.scss';
+
 const validationSchema = Yup.object({
   email: Yup.string()
     .email('Invalid email address')
-    .required('это обязательное поле'),
+    .required(<p className="formLog_ErrorMessage">это обязательное поле</p>),
   password: Yup.string()
     .min(6, 'Must be 6 characters or more')
     .max(20, 'Must be 20 characters or less')
-    .required('это обязательное поле'),
+    .required(<p className="formLog_ErrorMessage">это обязательное поле</p>),
 });
 
 export default function LoginForm({ handleSubmitLogin }) {
@@ -24,33 +26,50 @@ export default function LoginForm({ handleSubmitLogin }) {
         resetForm();
       }}
     >
-      <Form autoComplete="off">
-        <label htmlFor="email">Электронная почта: </label>
-        <br />
-        <Field
-          type="email"
-          name="email"
-          placeholder="your@email.com"
-          onChenge="submit"
-        />
-        <br />
-        <ErrorMessage name="email" />
-        <br />
-        <label htmlFor="password">Пароль: </label>
-        <br />
-        <Field
-          type="password"
-          name="password"
-          placeholder="******"
-          onChenge="submit"
-        />
-        <br />
-        <ErrorMessage name="password" />
-        <br />
-        <button type="submit">Войти</button>
-        <NavLink to="/register" exact>
-          Регистрация
-        </NavLink>
+      <Form autoComplete="off" className="formLog">
+        <p className="formLog_text google">
+          Вы можете авторизоваться с помощью Google Account:
+        </p>
+        {/* Здесь должен быть батон Гугл */}
+        <p className="formLog_text ">
+          Или зайти с помощью e-mail и пароля, предварительно
+          зарегистрировавшись:
+        </p>
+        <label htmlFor="email" className="formLog_label">
+          <span className="formLog_redStar">*</span>Электронная почта:
+          colin@mail.com{' '}
+        </label>
+        <div className="formLog_input">
+          <Field
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            className="formLog_placeholder"
+          />
+          <ErrorMessage name="email" />
+        </div>
+        <label htmlFor="password" className="formLog_label">
+          <span className="formLog_redStar">*</span>Пароль: Colin111111{' '}
+        </label>
+        <div className="formLog_input_last">
+          <Field
+            type="password"
+            name="password"
+            placeholder="******"
+            className="formLog_placeholder"
+          />
+          <ErrorMessage name="password" />
+        </div>
+        <div className="formLog_footer">
+          <button type="submit" className="formLog_btn">
+            Войти
+          </button>
+          <NavLink to="/register" exact className="formLog_NavLink">
+            <button type="button" className="formLog_btn">
+              Регистрация
+            </button>
+          </NavLink>
+        </div>
       </Form>
     </Formik>
   );
