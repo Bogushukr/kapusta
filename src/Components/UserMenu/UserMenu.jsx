6267579b7d27ffc86447e7535e8383b04f95af18
Yuliia Burko <yuliia.burko@gmail.com>
2021-10-12 21:57:11 +0200

    add logout modal

--- src/Components/UserMenu/UserMenu.jsx
+++ src/Components/UserMenu/UserMenu.jsx
@@ -1,8 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors, authOperations } from '../../Redux/auth';
 import UserAvatar from '../../Icons/userAvatar.svg';
 import style from './UserMenu.module.css';
+import Modal from '../Modal/Modal';
 
 const UserMenu = () => {
   const name = useSelector(authSelectors.getUsername);
@@ -10,13 +11,27 @@ const UserMenu = () => {
   const logOut = useCallback(() => {
     dispatch(authOperations.logOut());
   }, [dispatch]);
+
+  const [setOpenModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
+  };
   return (
     <div className={style.container}>
       <img src={UserAvatar} alt="" width="32" className={style.avatar} />
       <span className={style.name}>Welcome, {name}</span>
-      <button type="button" onClick={logOut}>
+      <button type="button" onClick={toggleModal}>
         Выйти
       </button>
+      {setOpenModal && (
+        <Modal
+          text={'Вы действительно хотите выйти?'}
+          handleConfirmClick={logOut}
+          handleRejectClick={toggleModal}
+          onModalClose={toggleModal}
+        />
+      )}
     </div>
   );
 };
