b330253a856fea5e5bae447ef9f80eb128d0ac7e
ivanberezin <58605673+ivanberezin@users.noreply.github.com>
2021-10-14 07:02:38 +0300

    exp-inc_add_calendar

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Balance from '../../Components/Balance'
 import SectionExpense from '../../Sections/SectionExpense/SectionExpense'
 import SectionIncome from '../../Sections/SectionIncome/SectionIncome'
+import Calendar from '../../Components/Calendar/Calendar';
 
 const HomeView = () => {
   const path = useLocation().pathname
@@ -12,11 +13,11 @@ const HomeView = () => {
       <Balance />
       <div className="summary">
             <Link to="/home/expense" className="consumptionButton">Расход</Link>
-            <Link to="/home/income" exact className="incomeButton">Доход</Link>
+            <Link to="/home/income" className="incomeButton">Доход</Link>
       </div>
       <div>{`${path}`==='/home/income' ? <SectionIncome /> : <SectionExpense />} </div>
     </>
   )
-};
+}
 
 export default HomeView;
